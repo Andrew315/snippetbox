101d247c7560cc8ce1cdebd54ad07453df5c15fa
Andrew315 <andrey.osipov315@gmail.com>
2022-07-29 00:59:56 +0400

    added handlers

--- main.go
+++ main.go
@@ -7,13 +7,32 @@ import (
 
 // Home handler
 func home(w http.ResponseWriter, r *http.Request) {
+
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+
 	w.Write([]byte("Hello from Snippetbox"))
+}
+
+//showSnippet handler function
+func showSnippet(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a specific snippet..."))
+}
+
+//createSnippet handler function
+func createSnippet(w http.ResponseWriter, r *http.Request) {
+
+	w.Write([]byte("Create a new snippet..."))
 }
 
 // main function of program
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet", showSnippet)
+	mux.HandleFunc("/snippet/create", createSnippet)
 
 	// starting new web server
 	log.Println("Starting server on :4000")
